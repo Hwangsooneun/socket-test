a5601ee25037d6d6246f9b5da0e4b4f71474b3ea
Hwangsooneun <catalyst8849@gmail.com>
2021-07-06 08:33:26 +0900

    fixport

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const cors = require('cors')
 const app = express()
 
 const server = require('http').Server(app)
-const io = require('socket.io')(server, {
-  cors: {
-    origin: "https://kingdonkey.site",
-    methods: ["GET", "POST"]
-  }
-})
-const { v4: uuidV4 } = require('uuid')
+// const io = require('socket.io')(server, {
+//   cors: {
+//     origin: "https://kingdonkey.site",
+//     methods: ["GET", "POST"]
+//   }
+// })
+// const { v4: uuidV4 } = require('uuid')
 
-const PORT = 80;
+const PORT = 3000;
 
 
 app.use(cors({
@@ -26,39 +26,39 @@ app.get('/', (req, res) => {
   console.log(`방금 사이트에 유저 접속함`)
 })
 
-app.get('/chatroom', (req, res) => {
-  let roomId = uuidV4()
-  console.log(`서버가 만든 룸ID: ${roomId}`)
-  res.redirect(`/chatroom/${roomId}`)
-})
+// app.get('/chatroom', (req, res) => {
+//   let roomId = uuidV4()
+//   console.log(`서버가 만든 룸ID: ${roomId}`)
+//   res.redirect(`/chatroom/${roomId}`)
+// })
 
-app.get('/chatroom/:roomId', (req, res) => {
-  res.status(200).send({roomId: req.params.roomId})
-})
+// app.get('/chatroom/:roomId', (req, res) => {
+//   res.status(200).send({roomId: req.params.roomId})
+// })
 
-io.on('connection', (socket) => {
+// io.on('connection', (socket) => {
 
-  // 누군가가 방에 들어올 때마다, 클라가 서버에게 방아이디랑 유저 아이디를 서버에 전달함
-  // 그 전달받은 정보(roomId, userId)를 가지고
-  // 1. socket.join(roomId): 이름이 roomID인 방을 만듬
-  socket.on('join-room', (roomId, userId) => {
+//   // 누군가가 방에 들어올 때마다, 클라가 서버에게 방아이디랑 유저 아이디를 서버에 전달함
+//   // 그 전달받은 정보(roomId, userId)를 가지고
+//   // 1. socket.join(roomId): 이름이 roomID인 방을 만듬
+//   socket.on('join-room', (roomId, userId) => {
     
-    console.log(`누가 접속했다! 방번호: ${roomId}, 유저 아이디: ${userId}`)
+//     console.log(`누가 접속했다! 방번호: ${roomId}, 유저 아이디: ${userId}`)
 
-    // 받은 roomId를 가지고 방을 만듬
-    socket.join(roomId)
+//     // 받은 roomId를 가지고 방을 만듬
+//     socket.join(roomId)
 
-    // 새로운 유저가 이 방에 들어왔을 때,
-    // 그 유저의 ID를 그 방에 있는 다른 사람의 클라에 전달함.
-    // (그래야 그 새 유저의 영상을 이미 있던 사람들이 볼 수 있게 연결 시도할 수 있음)
-    socket.broadcast.to(roomId).emit('user-connected', userId)
+//     // 새로운 유저가 이 방에 들어왔을 때,
+//     // 그 유저의 ID를 그 방에 있는 다른 사람의 클라에 전달함.
+//     // (그래야 그 새 유저의 영상을 이미 있던 사람들이 볼 수 있게 연결 시도할 수 있음)
+//     socket.broadcast.to(roomId).emit('user-connected', userId)
     
-    socket.on('disconnect', () => {
-      console.log(`유저 떠났다!!! 떠난놈 ID: ${userId}`)
-      socket.broadcast.to(roomId).emit('user-disconnected', userId)
-    })
-  })
-})
+//     socket.on('disconnect', () => {
+//       console.log(`유저 떠났다!!! 떠난놈 ID: ${userId}`)
+//       socket.broadcast.to(roomId).emit('user-disconnected', userId)
+//     })
+//   })
+// })
 
 server.listen(PORT, () => {
   console.log(`서연: 서버는 현재 ${PORT}포트에서 돌아가는 중이다`)
