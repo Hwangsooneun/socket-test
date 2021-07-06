08b06f26c3f888a1a1088ee011410f20c5c640d6
Hwangsooneun <catalyst8849@gmail.com>
2021-07-06 13:57:45 +0900

    test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors({
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.status(200).send('CI/CD성공!!!!')
+  res.status(200).send('CI/CD성공!!!!2')
   console.log(`방금 사이트에 유저 접속함`)
 })
 
