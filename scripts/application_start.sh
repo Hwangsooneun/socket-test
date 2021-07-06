67a2e75ce135a9593f050e92c3bf4b1372305d00
Hwangsooneun <catalyst8849@gmail.com>
2021-07-06 14:32:23 +0900

    test

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,14 +7,6 @@ cd /home/ubuntu/server
 
 #install node modules
 npm install
-npm install pm2 -g
-sudo apt-get update
-sudo apt-get install authbind
-sudo touch /etc/authbind/byport/80
-sudo chown ubuntu /etc/authbind/byport/80
-sudo chmod 755 /etc/authbind/byport/80
-authbind --deep pm2 update
-
 
 #start our node app in the background(pm2)
 authbind --deep pm2 start index.js
