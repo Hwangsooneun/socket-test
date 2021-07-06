2f2075a4c1b3148a0b61209cb26efbab709d438c
Hwangsooneun <catalyst8849@gmail.com>
2021-07-06 14:25:45 +0900

    test

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,11 +5,6 @@ sudo chmod -R 777 /home/ubuntu/server
 #깃허브에서 받은 파일로 이동
 cd /home/ubuntu/server
 
-#npm, node path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
 #install node modules
 npm install
 npm install pm2 -g
